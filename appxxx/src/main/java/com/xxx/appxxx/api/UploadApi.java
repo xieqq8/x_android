d466d00ccc08ff41de59eac54adf5ad02ff06e8e
xieqingqing <xieqq8@163.com>
2016-12-16 16:49:46 +0800

    xie Fg300Me  网络操作，上传下载示例〜

---
+++ appxxx/src/main/java/com/xxx/appxxx/api/UploadApi.java
@@ -0,0 +1,44 @@
+package com.xxx.appxxx.api;
+
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
+import com.wzgiceman.rxretrofitlibrary.retrofit_rx.Api.BaseApi;
+import com.wzgiceman.rxretrofitlibrary.retrofit_rx.http.HttpService;
+import com.wzgiceman.rxretrofitlibrary.retrofit_rx.listener.HttpOnNextListener;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import rx.Observable;
+
+/**
+ * 上传请求api
+ * Created by WZG on 2016/10/20.
+ */
+
+public class UploadApi<T> extends BaseApi<T> {
+    /*需要上传的文件*/
+    private MultipartBody.Part part;
+
+
+    public UploadApi(HttpOnNextListener listener, RxAppCompatActivity rxAppCompatActivity) {
+        super(listener, rxAppCompatActivity);
+        setShowProgress(true);
+        setMothed("AppFiftyToneGraph/videoLink");
+    }
+
+    public MultipartBody.Part getPart() {
+        return part;
+    }
+
+    public void setPart(MultipartBody.Part part) {
+        this.part = part;
+    }
+
+    @Override
+    public Observable getObservable(HttpService methods) {
+        RequestBody uid= RequestBody.create(MediaType.parse("text/plain"), "4811420");
+        RequestBody key = RequestBody.create(MediaType.parse("text/plain"), "cfed6cc8caad0d79ea56d917376dc4df");
+        return methods.uploadImage(uid,key,getPart());
+    }
+
+}
