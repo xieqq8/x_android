94f6833f77bf3bb401b4c4d04c6a2492bdb7f6f8
xieqingqing <xieqq8@163.com>
2017-05-23 17:59:21 +0800

    xie

--- mvpdemo/src/androidTest/java/com/xxx/mvpdemo/ExampleInstrumentedTest.java
+++ mvpdemo/src/androidTest/java/com/xxx/mvpdemo/ExampleInstrumentedTest.java
@@ -10,6 +10,11 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
+ * 测试时需要使用 Android api支持
+ 测试时需要使用 Android 中的组件
+ 测试时需要访问 Android 中的特定环境元素（例如 Context)
+
+ 其他情况优先考虑使用 Unit Test ，因为它的速度要快很多，效率也要高很多。其次，Instrumented unit tes 是基于 Unit Test 的。所以我们先对 Unit Test 进行研究。
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
