69f629d188008bfc954c014a51542c7b5aa12e76
xie qingqing <xieqq8@sohu.com>
2017-05-31 23:55:54 +0800

    xie

--- XSwift/XSwift/ViewController.swift
+++ XSwift/XSwift/ViewController.swift
@@ -8,22 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
+    @IBOutlet weak var Label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func touchDown(_ sender: Any) {
+    
+        
+    }
+    
+    
+    @IBAction func onClick(_ sender: Any) {
+        self.Label1.text = "hello world"
+    }
+    
     @IBOutlet weak var helloButton: UIButton!
 
     @IBAction func showAlert(_ sender: Any) {
         self.helloButton.setTitle("clicked", for: UIControlState.normal)
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 }
 
