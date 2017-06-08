5e8443eb72d088b21f5326e0279ee79cc9ea08e9
xieqingqing <xieqq8@163.com>
2017-06-08 17:39:34 +0800

    xie swift  demo 构架学习

--- XSwift/XSwift/ViewController.swift
+++ XSwift/XSwift/ViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
-
-    @IBOutlet weak var Label1: UILabel!
+class ViewController: UITabBarController {
+//, UITextFieldDelegate, UITextViewDelegate
+//    @IBOutlet weak var Label1: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,26 +23,26 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate 
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func touchDown(_ sender: Any) {
+//    @IBAction func touchDown(_ sender: Any) {
+//    
+//        
+//    }
     
-        
-    }
-    
-    
-    @IBAction func onClick(_ sender: Any) {
-        self.Label1.text = "hello world"
-    }
-    
-    @IBOutlet weak var helloButton: UIButton!
-
-    @IBAction func showAlert(_ sender: Any) {
-        self.helloButton.setTitle("clicked", for: UIControlState.normal)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
+//    
+//    @IBAction func onClick(_ sender: Any) {
+//        self.Label1.text = "hello world"
+//    }
+//    
+//    @IBOutlet weak var helloButton: UIButton!
+//
+//    @IBAction func showAlert(_ sender: Any) {
+//        self.helloButton.setTitle("clicked", for: UIControlState.normal)
+//    }
+//    
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
     
     
 }
